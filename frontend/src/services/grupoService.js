6b023b6b455a7f692f34f5b20796e1dab487b688
gabrielcastroandrade <castroandradegabriel@gmail.com>
2026-05-30 11:04:03 -0300

    preparando a api do lado do front

---
+++ frontend/src/services/grupoService.js
@@ -0,0 +1,32 @@
+import api from './api';
+
+const grupoService = {
+  getListaGrupos: async () => {
+    const response = await api.get('/grupos');
+    return response.data;
+  },
+  
+  criarGrupo: async (nome, senha) => {
+    const response = await api.post('/grupos', { nome, senha });
+    return response.data;
+  },
+
+  // Alinhado com ServicoGrupos.excluirGrupo(UUID, String)
+  excluirGrupo: async (id, senha) => {
+    const response = await api.delete(`/grupos/${id}`, {
+      data: { senha }
+    });
+    return response.data;
+  },
+
+  // Alinhado com ServicoGrupos.pontuar(UUID, UUID, UUID)
+  registrarAtividade: async (grupoId, pessoaId, eventoId) => {
+    const response = await api.post(`/grupos/${grupoId}/pontuar`, { 
+      pessoaId, 
+      eventoId 
+    });
+    return response.data;
+  },
+};
+
+export default grupoService;
