e4e73edb1dc126565e68fb803a1a91fa54b1ef3b
Mantagute <joaogabrieliuzviakmantagute@gmail.com>
2026-06-05 15:24:12 -0300

    feat: gerenciamento de pessoas e eventos com adição e remoção pelo front

--- frontend/src/components/PaginaGrupo/PaginaGrupo.jsx
+++ frontend/src/components/PaginaGrupo/PaginaGrupo.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import GerenciadorPessoas from '../PaginaGrupo/GerenciadorPessoas';
+import GerenciadorEventos from './GerenciadorEventos';
 import grupoService from '../../services/grupoService';
 import pessoaService from '../../services/pessoaService';
 import eventoService from '../../services/eventoService';
@@ -53,42 +55,67 @@ function PaginaGrupo() {
     }
   }
 
+  const onAtualizarPessoas = async () => {
+    try {
+      const dadosPessoas = await pessoaService.getListaPessoas(id);
+      setPessoas(dadosPessoas);
+    }
+    catch (error) {
+      console.error('Erro ao atualizar pessoas:', error);
+    }
+  }
+
+  const onAtualizarEventos = async () => {
+    try {
+      const dadosEventos = await eventoService.getListaEventos(id);
+      setEventos(dadosEventos);
+    } catch (error) {
+      console.error("Erro ao atualizar eventos:", error);
+    }
+  }
+
+
+
   return (
-    <div className="pagina-grupo">
-      <div className="pagina-grupo-header">
-        <button className="pagina-grupo-voltar" onClick={() => navigate('/')}>
-          &lt;&lt; Voltar
-        </button>
+    <>
+      <div className="pagina-grupo">
+        <div className="pagina-grupo-header">
+          <button className="pagina-grupo-voltar" onClick={() => navigate('/')}>
+            &lt;&lt; Voltar
+          </button>
+        </div>
+        <div className="pagina-grupo-perfil">
+          <h1 className="pagina-grupo-titulo">{grupo.nome}</h1>
+          <h2 className="pagina-grupo-meta">
+            meta: {grupo.pontuacaoAtual}/{grupo.meta}
+          </h2>
+          <h3 className="pagina-grupo-categoria">categoria: {grupo.classificacao}</h3>
+        </div>
+        <div className="pagina-grupo-pontuar">
+          <select onChange={(e) => setPessoa(e.target.value)} value={pessoaSelecionada || ''}>
+            <option value="">Selecione uma pessoa...</option>
+            {pessoas.map((pessoa) => (
+              <option key={pessoa.id} value={pessoa.id}>
+                {pessoa.nome}
+              </option>
+            ))}
+          </select>
+    
+          <select onChange={(e) => setEvento(e.target.value)} value={eventoSelecionado || ''}>
+            <option value="">Selecione um evento...</option>
+            {eventos.map((evento) => (
+              <option key={evento.id} value={evento.id}>
+                {evento.nome}
+              </option>
+            ))}
+          </select>
+    
+          <button onClick={lidarPontuar}>Pontuar</button>
+        </div>
       </div>
-      <div className="pagina-grupo-perfil">
-        <h1 className="pagina-grupo-titulo">{grupo.nome}</h1>
-        <h2 className="pagina-grupo-meta">
-          meta: {grupo.pontuacaoAtual}/{grupo.meta}
-        </h2>
-        <h3 className="pagina-grupo-categoria">categoria: {grupo.classificacao}</h3>
-      </div>
-      <div className="pagina-grupo-pontuar">
-        <select onChange={(e) => setPessoa(e.target.value)} value={pessoaSelecionada || ''}>
-          <option value="">Selecione uma pessoa...</option>
-          {pessoas.map((pessoa) => (
-            <option key={pessoa.id} value={pessoa.id}>
-              {pessoa.nome}
-            </option>
-          ))}
-        </select>
-  
-        <select onChange={(e) => setEvento(e.target.value)} value={eventoSelecionado || ''}>
-          <option value="">Selecione um evento...</option>
-          {eventos.map((evento) => (
-            <option key={evento.id} value={evento.id}>
-              {evento.nome}
-            </option>
-          ))}
-        </select>
-  
-        <button onClick={lidarPontuar}>Pontuar</button>
-      </div>
-    </div>
+      <GerenciadorPessoas grupoId={id} pessoas={pessoas} onAtualizarPessoas={onAtualizarPessoas} />
+      <GerenciadorEventos grupoId={id} eventos = {eventos} onAtualizarEventos={onAtualizarEventos}/>
+    </>
   );
 }
 
